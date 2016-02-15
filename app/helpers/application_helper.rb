7142eb072823c0eaeef767f4762babf33adbc002
Faheem Zunjani <faheemzunjani@gmail.com>
2016-02-15 11:35:19 +0530

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		#Returns the full title on a per-page basis
+		base_title = "Sample Rails App"	
+
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
